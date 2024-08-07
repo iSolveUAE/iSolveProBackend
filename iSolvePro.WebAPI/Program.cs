b0c3a13e19907a764d452277c8e9f2a18bd0cd66
IshaFatima <ishaftaima40@gmail.com>
2024-08-07 15:32:49 +0500

    services module

--- iSolvePro.WebAPI/Program.cs
+++ iSolvePro.WebAPI/Program.cs
@@ -1,12 +1,14 @@
-var builder = WebApplication.CreateBuilder(args);
+using iSolvePro.Infrastructure.DIConfiguration;
 
+var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+ServiceModule.Register(services);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
