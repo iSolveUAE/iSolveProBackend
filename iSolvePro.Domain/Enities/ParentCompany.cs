74ad6bedd675ec7b446d843281292de64f9f1ff2
iSolveUAE <llcisolve@gmail.com>
2024-08-07 10:25:46 +0500

    Project Structure

---
+++ iSolvePro.Domain/Enities/ParentCompany.cs
@@ -0,0 +1,28 @@
+﻿using iSolvePro.Domain.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iSolvePro.Domain.Enities
+{
+    public class ParentCompany:Setup
+    {
+       
+
+        [Column(TypeName =("varchar(200)"))]
+        public string CompanyName { get; set; } = string.Empty;
+        [Column(TypeName = ("tinyint"))]
+        public byte Employees { get; set; }
+
+        [Column(TypeName = ("varchar(100)"))]
+        public string  Country { get; set; }=string.Empty;
+        [Column(TypeName = ("varchar(100)"))]
+        public string City { get; set; }=string.Empty;
+        [Column(TypeName = ("varchar(100)"))]
+        public string Website { get; set; } = string.Empty;
+    }
+}
---
+++ iSolvePro.Domain/Enities/SubCompany.cs
@@ -0,0 +1,26 @@
+﻿using iSolvePro.Domain.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iSolvePro.Domain.Enities
+{
+    public class SubCompany:Setup
+    {
+        public int ParentCompID { get; set; }
+        [Column(TypeName = ("varchar(200)"))]
+        public string CompanyName { get; set; } = string.Empty;
+        [Column(TypeName = ("tinyint"))]
+        public byte Employees { get; set; }
+
+        [Column(TypeName = ("varchar(100)"))]
+        public string Country { get; set; } = string.Empty;
+        [Column(TypeName = ("varchar(100)"))]
+        public string City { get; set; } = string.Empty;
+        [Column(TypeName = ("varchar(100)"))]
+        public string Website { get; set; } = string.Empty;
+    }
+}
